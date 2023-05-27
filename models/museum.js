65de6bf092bbe4e2740c822262d9d737df0ff16b
Mayte Ozoria <mayteozoria@gmail.com>
2023-05-26 22:38:21 -0400

    created the schema model

---
+++ models/museum.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const museumSchema = new Schema({
+  title: String,
+  location: String,
+
+  openingTime: {
+    type: String,
+    required: trusted
+  },
+  closingTIme: {
+    type: String,
+    required: true
+  },
+  addmissionCost: Number
+})
+
+// Compile the schema into a model and export it
+module.exports = mongoose.model('Museum', museumSchema)
