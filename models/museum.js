96192456ad7b38a1cd0b3de3ce02dda7159038f7
Mayte Ozoria <mayteozoria@gmail.com>
2023-05-26 23:51:56 -0400

    added OAUTH and DB

--- models/museum.js
+++ models/museum.js
@@ -2,20 +2,25 @@ const mongoose = require('mongoose')
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
 
-const museumSchema = new Schema({
-  title: String,
-  location: String,
-
-  openingTime: {
-    type: String,
-    required: trusted
+const museumSchema = new Schema(
+  {
+    title: String,
+    location: String,
+    openingTime: {
+      type: String,
+      required: trusted
+    },
+    closingTIme: {
+      type: String,
+      required: true
+    },
+    currentExhibit: String,
+    addmissionCost: Number
   },
-  closingTIme: {
-    type: String,
-    required: true
-  },
-  addmissionCost: Number
-})
+  {
+    timestamps: trueexit
+  }
+)
 
 // Compile the schema into a model and export it
 module.exports = mongoose.model('Museum', museumSchema)
